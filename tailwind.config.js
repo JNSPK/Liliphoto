6bc20ca1aeb305084993c875259d0bcc9b9c415e
Johan Nilsson <johan.nilsson.mail@gmail.com>
2024-08-06 09:33:13 +0200

    feat(style): font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
         '2xl': '1400px',
       },
     },
+    fontFamily: {
+      quicksand: ['Quicksand', 'sans-serif'],
+    },
     extend: {
       colors: {
         border: 'hsl(var(--border))',
